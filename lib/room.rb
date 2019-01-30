8d942d1049b8967fe2ed9854ce5f408a11e97a58
Paul Schlattmann <paul.h.schlattmann@gmail.com>
2019-01-30 09:24:54 -0700

    Added: Room and Room Test/Iteration 1

---
+++ lib/room.rb
@@ -0,0 +1,15 @@
+class Room
+  attr_reader :category, :area
+
+  def initialize(category, width, length)
+    @category = category
+    @area = width * length
+  end
+
+
+
+
+
+
+
+end
---
+++ test/room_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+
+class RoomTest < Minitest::Test
+
+def test_room_exists
+  room = Room.new(:bedroom, 10, 13)
+
+  assert_instance_of Room, room
+end
+
+def test_room_has_category
+  room = Room.new(:bedroom, 10, 13)
+
+  assert_equal :bedroom, room.category
+end
+
+def test_room_has_area
+  room = Room.new(:bedroom, 10, 13)
+
+  assert_equal 130, room.area
+end
+
+
+end
+
+
+
+
+
+#
